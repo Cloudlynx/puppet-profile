32cd1e9a034fb5888710e3891a4f5c4159acdb54
Michael Gruener <michael.gruener@cloudlynx.ch>
2015-03-07 18:46:50 +0100

    Add certificate creation

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,12 @@
 class profile::base (
   $sshd_options = {},
   $sshd_subsystems = {},
+  $certificates = {},
 ) {
+  include certtool
+
   create_resources('sshd_config',$sshd_options)
   create_resources('sshd_config_subsystem',$sshd_subsystems)
+
+  create_resources('certtool::cert',$certificates)
 }
